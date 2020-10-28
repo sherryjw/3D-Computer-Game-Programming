2b73cff54c16d401f4154d643290d41f8115e1c7
sherryjw <43445923+sherryjw@users.noreply.github.com>
2020-10-28 19:15:31 +0800

    Add files via upload

---
+++ Homework5/Assets/Scripts/DiskFlyAction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiskFlyAction : SSAction {
+    public float gravity = -0.2f;
+    private Vector3 speed;
+    private Vector3 gravityVector = Vector3.zero;
+    private Vector3 angle = Vector3.zero;
+    private float time;
+
+    private DiskFlyAction() { }
+    public static DiskFlyAction GetSSAction(int direction, float angle, float speed) {
+        DiskFlyAction action = ScriptableObject.CreateInstance<DiskFlyAction>();
+        if (direction == -1) {
+            action.speed = Quaternion.Euler(new Vector3(0, 0, -angle)) * Vector3.left * speed;
+        }
+        else {
+            action.speed = Quaternion.Euler(new Vector3(0, 0, angle)) * Vector3.right * speed;
+        }
+        return action;
+    }
+
+    public override void Start() { }
+
+    public override void Update() {
+        time += Time.deltaTime;
+        gravityVector.y = gravity * time;
+
+        transform.position += (speed + gravityVector) * Time.fixedDeltaTime;
+        angle.z = Mathf.Atan((speed.y + gravityVector.y) / speed.x) * Mathf.Rad2Deg;
+        transform.eulerAngles = angle;
+
+        if (this.transform.position.y < -20f) {
+            this.destroy = true;
+            this.callback.SSActionEvent(this);      
+        }
+    }
+}
---
+++ Homework5/Assets/Scripts/FlyActionManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyActionManager : SSActionManager {
+    public DiskFlyAction flyAction;  
+    public FirstController sceneController;           
+
+    protected void Start() {
+        sceneController = (FirstController)SSDirector.GetInstance().CurrentSceneController;
+        sceneController.actionManager = this;     
+    }
+
+    public void DiskFly(GameObject disk, float angle, float speed) {
+        int direction = (disk.transform.position.x > 0) ? -1 : 1;
+        flyAction = DiskFlyAction.GetSSAction(direction, angle, speed);
+        this.RunAction(disk, flyAction, this);
+    }
+}
