e14f30a439a835a5d7e18a0b83ca9c397ad83668
sherryjw <43445923+sherryjw@users.noreply.github.com>
2020-11-11 20:01:31 +0800

    Add files via upload

--- Homework6/Assets/Scripts/DiskFlyPhysicsAction.cs
+++ Homework6/Assets/Scripts/DiskFlyPhysicsAction.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -25,9 +25,9 @@ public class DiskFlyPhysicsAction : SSAction
 
         float x = Random.Range(-15f, 15f), y = Random.Range(15f, 20f), z = Random.Range(5f, 10f);
         disk.transform.position = new Vector3(x, y, z);
-        DiskFlyPhysicsAction act = CreateInstance<DiskFlyPhysicsAction>();
-        act.speed = Quaternion.Euler(new Vector3(0, 0, angle)) * Vector3.right * power;
-        return act;
+        DiskFlyPhysicsAction action = CreateInstance<DiskFlyPhysicsAction>();
+        action.speed = Quaternion.Euler(new Vector3(0, 0, angle)) * Vector3.right * power;
+        return action;
     }
 
     public override void Update() { }
