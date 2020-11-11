f0c94dde428ac1732c20fb9991c206209d78a1c0
sherryjw <43445923+sherryjw@users.noreply.github.com>
2020-11-11 20:05:25 +0800

    Add files via upload

--- Homework6/Assets/Scripts/FlyActionManager.cs
+++ Homework6/Assets/Scripts/FlyActionManager.cs
@@ -1,27 +1,28 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlyActionManager : SSActionManager {
-    /*public DiskFlyAction flyAction;*/
-    public SSAction flyAction;
+public class FlyActionManager : SSActionManager, IActionManager, ISSActionCallback
+{
+    public DiskFlyAction flyAction;
     public FirstController sceneController;           
 
     protected void Start() {
         sceneController = (FirstController)SSDirector.GetInstance().CurrentSceneController;
-        sceneController.actionManager = this;     
     }
 
     public void DiskFly(GameObject disk, float angle, float power) {
         int direction = (disk.transform.position.x > 0) ? -1 : 1;
-        if (sceneController.isPhysics())
-        {
-            flyAction = DiskFlyPhysicsAction.GetSSAction(disk, angle, power);
-        }
-        else
-        {
-            flyAction = DiskFlyAction.GetSSAction(direction, angle, power);
-        }
+        flyAction = DiskFlyAction.GetSSAction(direction, angle, power);
         this.RunAction(disk, flyAction, this);
+    }
+
+    public void SSActionEvent(SSAction source,
+    SSActionEventType events = SSActionEventType.Competeted,
+    int intParam = 0,
+    string strParam = null,
+    Object objectParam = null)
+    {
+        sceneController.FreeDisk(source.gameobject);
     }
 }
