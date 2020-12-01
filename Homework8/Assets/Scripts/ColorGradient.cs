ce006bab83f983363db654a1a4bc39e9b084c65e
sherryjw <43445923+sherryjw@users.noreply.github.com>
2020-12-01 16:07:42 +0800

    Add files via upload

---
+++ Homework8/Assets/Scripts/ColorGradient.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorGradient : MonoBehaviour
+{
+    private ParticleSystem ball;
+    private float r, g, b;
+    private bool flag = true;
+    
+    void Start()
+    {
+        ball = this.GetComponent<ParticleSystem>();
+        r = 1.0f;
+        g = 0.3f;
+        b = 0.25f;
+    }
+
+    void Update()
+    {
+        if (g >= 0.95f)
+        {
+            flag = false;
+        }
+        else if (g <= 0.3f)
+        {
+            flag = true;
+        }
+
+        if (flag)
+        {
+            g += 0.001f;
+        }
+        else
+        {
+            g -= 0.001f;
+        }
+        var main = ball.main;
+        main.startColor = new ParticleSystem.MinMaxGradient(new Color(r, g, b));
+    }
+}
---
+++ Homework8/Assets/Scripts/Sky.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sky : MonoBehaviour
+{
+    void Start()
+    {
+        for (int i = 0; i < 100; ++i)
+        {
+            ParticleSystem star = Instantiate(Resources.Load<ParticleSystem>("Prefabs/Star"), new Vector3(Random.Range(-14.0f, 14.0f), Random.Range(3.5f, 7.0f), 0), Quaternion.identity);
+        }
+    }
+}
